316b03f7d223f5367c18c5bc004acb96a421cc36
Munn Light <m.elbegsaikhan01@gmail.com>
2024-02-21 17:49:47 +0800

    moo

--- main.js
+++ main.js
@@ -1,50 +1,24 @@
-import * as THREE from 'three';
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
 
-const scene = new THREE.Scene();
+document.querySelector("#app").innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
 
-
-
-// Create axes helper
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-
-// Create grid helper
-const size = 100;
-const divisions = 100;
-const gridHelper = new THREE.GridHelper(size, divisions);
-scene.add(gridHelper);
-
-// Set initial camera position
-camera.position.z = 5;
-camera.position.y = 3;
-camera.position.x = 0;
-
-// Define function to animate the scene
-function animate() {
-    // Request animation frame for smoother animation
-    requestAnimationFrame(animate);
-
-    // Update camera position based on keyboard input
-    if (keyboard['ArrowUp']) {
-        camera.position.z -= 0.1;
-    }
-    if (keyboard['ArrowDown']) {
-        camera.position.z += 0.1;
-    }
-    if (keyboard['ArrowLeft']) {
-        camera.position.x -= 0.1;
-    }
-    if (keyboard['ArrowRight']) {
-        camera.position.x += 0.1;
-    }
-
-    // Make the camera always look at the center of the scene
-    camera.lookAt(scene.position);
-
-    // Render the scene
-    renderer.render(scene, camera);
-}
-
-// Start the animation
-animate();
->>>>>>> 3209d097b5dfe4f4407559ba116da53054ba1726
+setupCounter(document.querySelector("#counter"));
