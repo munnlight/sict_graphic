6c9f9176d71b122aace8ed469ac3d88e0618c9a1
Munn Light <m.elbegsaikhan01@gmail.com>
2024-02-22 16:25:46 +0800

    Дахин туршиж үзэв.

--- main.js
+++ main.js
@@ -2,27 +2,21 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 
-// Create axes helper
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-// Create grid helper
 const size = 100;
 const divisions = 100;
 const gridHelper = new THREE.GridHelper(size, divisions);
 scene.add(gridHelper);
 
-// Set initial camera position
 camera.position.z = 5;
 camera.position.y = 3;
 camera.position.x = 0;
 
-// Define function to animate the scene
 function animate() {
-  // Request animation frame for smoother animation
   requestAnimationFrame(animate);
 
-  // Update camera position based on keyboard input
   if (keyboard["ArrowUp"]) {
     camera.position.z -= 0.1;
   }
@@ -36,12 +30,8 @@ function animate() {
     camera.position.x += 0.1;
   }
 
-  // Make the camera always look at the center of the scene
   camera.lookAt(scene.position);
-
-  // Render the scene
   renderer.render(scene, camera);
 }
 
-// Start the animation
 animate();
