d6a03b8bf9d1ba342a237821a3cf455b3c44cdba
Temuujin <temuujintserenjav@gmail.com>
2024-02-26 13:46:40 +0800

    haalga turshih

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
 import * as THREE from "three";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
-
-renderer.setClearColor(0x0000ca);
-
+renderer.setClearColor(0x000000);
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
 
-const keyboard = {};
+const loader = new GLTFLoader();
 
+// Load a glTF resource using a relative path accessible from the web server
+loader.load(
+    'door22.glb', // or 'blender/untitled.glb' depending on your directory structure
+    function (gltf) {
+        const model = gltf.scene;
+
+        scene.add(model);
+    },
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+        console.log('An error happened', error);
+    }
+);
+
+const light = new THREE.DirectionalLight(0xffffff,10);
+light.position.set(5,5,5);
+scene.add(light);
+
+const light1 = new THREE.DirectionalLight(0xffffff,10);
+light1.position.set(-5,-5,-5);
+scene.add(light1);
+
+const keyboard = {};
 document.addEventListener('keydown', event => {
     keyboard[event.key] = true;
 });
@@ -21,30 +42,13 @@ document.addEventListener('keyup', event => {
     keyboard[event.key] = false;
 });
 
-const axesHelper = new THREE.AxesHelper(5);
+const axesHelper = new THREE.AxesHelper(10);
 scene.add(axesHelper);
 
 const size = 100;
 const divisions = 100;
 const gridHelper = new THREE.GridHelper(size, divisions);
 scene.add(gridHelper);
-
-const geometry = new THREE.BoxGeometry( 30, 0.1, 30); 
-const material = new THREE.MeshBasicMaterial( {color: 0xff8000, wireframe:false} ); 
-const cube = new THREE.Mesh( geometry, material ); 
-scene.add( cube );
-
-const geometryBlock = new THREE.BoxGeometry(1,10,30); 
-const materialBlock = new THREE.LineDashedMaterial( {
-	color: 0xffffff,
-	linewidth: 1,
-	scale: 1,
-	dashSize: 3,
-	gapSize: 1,
-} );
-const Block = new THREE.Mesh( geometryBlock, materialBlock ); 
-Block.position.set(-15,5,0);
-scene.add( Block );
 
 camera.position.z = 5;
 camera.position.y = 3;
