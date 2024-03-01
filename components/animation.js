714f603e00bc48eca6830aba7a1b3a3168a38abf
Munn Light <m.elbegsaikhan01@gmail.com>
2024-03-01 15:14:46 +0800

    Камера өөрчлөв.

--- components/animation.js
+++ components/animation.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { scene, camera, renderer } from "./outside";
 
-camera.position.set(0, 105, 115);
+camera.position.set(0, 105, 0);
 camera.up = new THREE.Vector3(0, 0, 1);
 
 const keyboard = {};
@@ -12,31 +12,39 @@ document.addEventListener("keyup", (event) => {
   keyboard[event.key] = false;
 });
 
+const radian = 180 / Math.PI;
+
+camera.lookAt(scene.position);
 function animate() {
   requestAnimationFrame(animate);
-
+  console.log(camera.rotation.x);
   if (keyboard["ArrowUp"]) {
-    camera.position.z -= 4;
+    camera.position.y += camera.rotation.x * Math.cos(camera.rotation.y);
+    camera.position.x += camera.rotation.x * Math.sin(camera.rotation.y);
   }
   if (keyboard["ArrowDown"]) {
-    camera.position.z += 4;
+    camera.position.y -= camera.rotation.x * Math.cos(camera.rotation.y);
+    camera.position.x -= camera.rotation.x * Math.sin(camera.rotation.y);
   }
   if (keyboard["ArrowLeft"]) {
-    camera.position.x -= 4;
+    camera.rotateY(Math.PI / 180);
   }
   if (keyboard["ArrowRight"]) {
-    camera.position.x += 4;
+    camera.rotateY(-Math.PI / 180);
   }
   if (keyboard["w"]) {
-    camera.position.y -= 4;
+    camera.position.z += 1;
   }
   if (keyboard["s"]) {
-    camera.position.y += 4;
+    camera.position.z -= 1;
   }
-
-  camera.lookAt(scene.position);
-
   renderer.render(scene, camera);
+  // var direction = new THREE.Vector3(0, 0, -1);
+  // direction.applyQuaternion(camera.quaternion);
+  // // Scale the direction vector by the distance to move
+  // direction.multiplyScalar(1);
+  // // Move the camera along the direction vector
+  // camera.position.add(direction);
 }
 
 animate();
