1d8421d0f0a139cef1a9c55579e971314bf5349e
nymaa <dorjderemnymdorj81@gmail.com>
2024-03-03 20:39:23 +0800

    dawhar 3iig nemj baiguulaw -nymaa

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 import "./style.css";
 import * as THREE from "three";
 import { scene } from "./components/outside";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { innerWall } from "./components/innerwall";
+//import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
 import { outerWall } from "./components/outerwall";
-import { Rooms } from "./components/rooms";
-import { combinedStairs } from "./components/stairs";
 import { animate } from "./components/animation";
-import {  floor1, floor2 } from "./components/floor";
+import {  floor1, floor2, floor3 } from "./components/floor";
 
 const fl1 = floor1;
 const fl2 = floor2;
+const fl3 = floor3;
 
 scene.add(fl1);
 scene.add(fl2);
+scene.add(fl3);
 
 const outwall = outerWall;
 scene.add(outwall);
